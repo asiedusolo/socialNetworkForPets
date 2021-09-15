6ca82c1542f87bf8f926726e382ed1115aa121f9
asiedusolo <solomon.asiedu@amalitech.org>
2021-09-15 09:09:59 -0400

    adding directory, profile, userlist and dataFetcher

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import './App.css';
+import React from 'react';
+import { Profile } from './Profile';
+import { Directory } from './Directory';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Socia network for pets</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUsername: null,
+    };
+    this.handleChoose = this.handleChoose.bind(this);
+    this.handleReturnToDirectoryClick = this.handleReturnToDirectoryClick.bind(
+      this
+    );
+  }
+
+  handleChoose(newUsername) {
+    this.setState({ currentUsername: newUsername });
+  }
+
+  handleReturnToDirectoryClick() {
+    this.setState({ currentUsername: null });
+  }
+
+  render() {
+    let body;
+    if (this.state.currentUsername) {
+      body = (
+        <Profile
+          username={this.state.currentUsername}
+          onChoose={this.handleChoose}
+        />
+      );
+    } else {
+      body = <Directory onChoose={this.handleChoose} />;
+    }
+
+    return (
+      <div className="App">
+        <header>
+          <h1>PetBook</h1>
+
+          <nav>
+            {this.state.currentUsername && (
+              <button onClick={this.handleReturnToDirectoryClick}>
+                Return to directory
+              </button>
+            )}
+          </nav>
+        </header>
+
+        <main>{body}</main>
+      </div>
+    );
+  }
 }
+
 
 export default App;
